0a819f85edc9b6ac2a00a877f2b09e3189adbbc4
TianWalkzzMiku <92043456+TianWalkzzMiku@users.noreply.github.com>
2023-01-18 13:19:13 +0700

    Update build-tc.sh

--- build-tc.sh
+++ build-tc.sh
@@ -102,6 +102,8 @@ tg_post_msg "<b>$LLVM_NAME: Toolchain compilation Finished</b>%0A<b>Clang Versio
 
 # Push to GitHub
 # Update Git repository
+git config --global user.email "whysdevsrk@gmail.com"
+git config --global user.name "TianWalkzzMiku"
 git clone "https://gitlab.com/TianWalkzzMiku/SuperRyzen-Clang.git" rel_repo
 pushd rel_repo || exit
 rm -fr ./*
